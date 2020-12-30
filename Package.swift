4629fb7f67530e73ca94f3447200c4765d195cd5
Daniel Horvath <daniel.horvath@tailored-apps.com>
2020-12-30 20:14:44 +0100

    add Package.swift

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version:5.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Modamic",
+    products: [
+        .library(
+            name: "Modamic",
+            targets: ["Modamic"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "Modamic",
+            dependencies: [],
+            path: "Sources"
+        ),
+        .testTarget(
+            name: "ModamicTests",
+            dependencies: ["Modamic"],
+            path: "Tests"
+        )
+    ]
+)
