addc0631fab074847ca85ca7213c8638db60552e
Daniel Horvath <daniel.horvath@tailored-apps.com>
2020-12-30 20:40:28 +0100

    update Package file

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -15,7 +15,8 @@ let package = Package(
         .target(
             name: "Modamic",
             dependencies: [],
-            path: "Sources/Modamic"
+            path: "Sources/Modamic",
+            exclude: ["Example","fastlane"]
         ),
         .testTarget(
             name: "ModamicTests",
